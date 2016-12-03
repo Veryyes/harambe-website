9fc72fb58266c4b6801216d528279c0a3dcdcde7
Brandon <veryyes10@yahoo.com>
2016-12-03 18:04:38 -0500

    see memories

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 import web
 from web import form
+import time
+import urllib
 
 render = web.template.render('templates/', base='layout')
+
 urls = (
 	'/','index', '/about.html', 'about', '/memories.html', 'memories', '/tragedy.html', 'tragedy'
 )
+
+db = web.database(dbn='mysql', user='harambe', pw='love', db='harambe')
 
 app = web.application(urls, globals())
 
@@ -22,15 +27,23 @@ class about:
 
 class memories:
 	def GET(self):
+		table = db.select('memories')
 		remember = entry()
 		#print remember.render()
-		return render.memories(remember)
+		return render.memories(remember, table)
 
 	def POST(self):
+		
 		remember = entry()
 		comment = web.data()[8:]
-		
-		return render.memories(remember)
+		print comment
+		comment = urllib.unquote(comment).decode('ascii')
+		print comment
+		table = db.select('memories')
+		date = time.strftime('%Y-%m-%d %X')
+		n = db.insert('memories', time=date,name='n/a',message=comment)
+		raise web.seeother('/memories.html')
+		#return render.memories(remember, table)
 
 class tragedy:
 	def GET(self):
