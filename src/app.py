e92031d62a71e2b2d107e2119d36354bb7575819
Brandon <veryyes10@yahoo.com>
2016-12-03 15:22:15 -0500

    submision form

--- src/app.py
+++ src/app.py
@@ -1,19 +1,35 @@
 import web
+from web import form
 
 render = web.template.render('templates/', base='layout')
 urls = (
-	'/','index', '/about', 'about'
+	'/','index', '/about.html', 'about', '/memories.html', 'memories', '/tragedy.html', 'tragedy'
 )
 
 app = web.application(urls, globals())
 
+entry = form.Form(
+	form.Textbox("remember"),
+	form.Button("submit")
+)
+
 class index:
 	def GET(self):
-		
 		return render.home()
+
 class about:
 	def GET(self):
-		return "about"
+		return render.about()
+
+class memories:
+	def GET(self):
+		remember = entry()
+		#print remember.render()
+		return render.memories(remember)
+
+class tragedy:
+	def GET(self):
+		return render.tragedy()
 
 if __name__=="__main__":
 	app.run()
